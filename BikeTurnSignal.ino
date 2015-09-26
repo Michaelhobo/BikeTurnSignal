f030c7fe10306cf6f2b7df00f3c7b426ed2e26aa
Michael Ho <callmemikeh@gmail.com>
2015-09-26 13:30:15 -0700

    basic changes

--- BikeTurnSignal.ino
+++ BikeTurnSignal.ino
@@ -1,29 +1,77 @@
-int on_coil = 8;
-int off_coil = 9;
+#include <helper_3dmath.h>
+#include <MPU6050.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050_9Axis_MotionApps41.h>
+
+#include <I2Cdev.h>
+
+#include <Wire.h>
+
+int EL_toggle = 9;
+int x_accel = A1;
+int y_accel = A2;
+int z_accel = A3;
+
+// The bias specifies the smallest value for a still sensor reading
+int x_bias = 401;
+int y_bias = 401;
+int z_bias = 391;
+
+// The value of gravity in one direction
+double g_x = 204.0;
+double g_y = 206.0;
+double g_z = 206.0;
+
+int panel_on = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(on_coil, OUTPUT);
-  pinMode(off_coil, OUTPUT);
+  pinMode(EL_toggle, OUTPUT);
+  pinMode(x_accel, INPUT);
+  pinMode(y_accel, INPUT);
+  pinMode(z_accel, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  EL_on();
-  delay(1000);
-  EL_off();
-  delay(1000);
+  double x = (analogRead(x_accel) - x_bias)/g_x;
+  double y = (analogRead(y_accel) - y_bias)/g_y;
+  double z = (analogRead(z_accel) - z_bias)/g_z;
+  if (z_up(z)) {
+    panel_on = 1;
+    EL_on();
+  } else {
+    EL_off();
+    panel_on = 0;
+  }
+  String output = "x: ";
+  output += x;
+  output += ", y: ";
+  output += y;
+  output +=", z: ";
+  output += z;
+  output += "\n";
+  Serial.print(output);
+}
+
+// find closeness to given angle
+
+/* Whether the z axis is facing "up" */
+bool z_up(int z) {
+  if (panel_on) {
+    return (z > 60);
+  } else {
+    return (z > 80);
+  }
 }
 
 /* Turn EL panel on */
 void EL_on() {
-  digitalWrite(off_coil, LOW);
-  digitalWrite(on_coil, HIGH);
+  digitalWrite(EL_toggle, HIGH);
 }
 
 /* Turn El panel off */
 void EL_off() {
-  digitalWrite(on_coil, LOW);
-  digitalWrite(off_coil, HIGH);
+  digitalWrite(EL_toggle, LOW);
 }
 
